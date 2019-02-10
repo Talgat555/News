2ee5c77967013ff39ad0c2f4b28451aa763b6295
unknown <tr-musin@ya.ru>
2019-02-10 14:17:56 +0600

    first

---
+++ src/components/news.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Article from './article';
+
+class News extends Component {
+  renderNews = () => {
+    const { data } = this.props
+    let newsTemplate = null
+      if (data.length) {
+        newsTemplate = data.map(function(item) {
+          return <Article key={item.id} data={item}/>
+        })
+      } else {
+          newsTemplate = <p>К сожалению новостей нет.</p>
+      }
+    return newsTemplate
+  }
+  render() {
+    const { data } = this.props
+    return (
+      <div className="news">
+        {this.renderNews()}
+        { data.length ?
+          <strong className={'news__count'}>
+             Всего новостей: {data.length}
+          </strong> : null
+        }
+      </div>
+    )
+  }
+}
+
+News.propTypes = {
+  data: PropTypes.array.isRequired 
+}
+
+export default News;
